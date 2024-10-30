3d22f587878c9e374ede4e3ff247922ab61dfb3e
CamilingJS <jonathancamiling@gmail.com>
2024-10-30 12:05:43 -0700

    cleaned up create tables operation file and loaded Big Instagram Data

--- Instagram_Clone.sql
+++ Instagram_Clone.sql
@@ -1,20 +1,12 @@
+DROP DATABASE IF EXISTS ig_clone;
 CREATE DATABASE ig_clone;
 USE ig_clone; 
 
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE users (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
     created_at TIMESTAMP DEFAULT NOW()
 );
-
-DESC users;
-
-INSERT INTO users (username) VALUES
-('CamilingJS'),
-('JayCamDev'),
-('DataMan');
-
-SELECT * FROM users; 
 
 CREATE TABLE photos (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -22,35 +14,17 @@ CREATE TABLE photos (
     user_id INTEGER NOT NULL,
     created_at TIMESTAMP DEFAULT NOW(),
     FOREIGN KEY(user_id) REFERENCES users(id)
-)
-
-SHOW tables;
-DESC photos;
-
-INSERT INTO photos (image_url, user_id) VALUES
-('/asldasd76', 1),
-('/asldasd66', 2),
-('/asldasd67', 2);
-
-SELECT * FROM photos
-JOIN users
-    ON photos.user_id = users.id; 
-
+);
 
 CREATE TABLE comments (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     comment_text VARCHAR(255) NOT NULL,
-    user_id INTEGER NOT NULL,
     photo_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
     created_at TIMESTAMP DEFAULT NOW(),
-    FOREIGN KEY(user_id) REFERENCES users(id),
-    FOREIGN KEY(photo_id) REFERENCES photos(id)
+    FOREIGN KEY(photo_id) REFERENCES photos(id),
+    FOREIGN KEY(user_id) REFERENCES users(id)
 );
-
-INSERT INTO comments(comment_text, user_id, photo_id) VALUES
-('I love it!', 1, 2),
-('Thank you!', 2, 2),
-('Cool!', 3, 1);
 
 CREATE TABLE likes (
     user_id INTEGER NOT NULL,
@@ -61,63 +35,27 @@ CREATE TABLE likes (
     PRIMARY KEY(user_id, photo_id)
 );
 
-INSERT INTO likes(user_id, photo_id) VALUES
-(1,1),
-(2,1),
-(1,3),
-(3,3);
-
-
--- Relationship tables
-CREATE TABLE follow (
+CREATE TABLE follows (
     follower_id INTEGER NOT NULL,
     followee_id INTEGER NOT NULL,
     created_at TIMESTAMP DEFAULT NOW(),
-    FOREIGN KEY (follower_id) REFERENCES users(id),
-    FOREIGN KEY (followee_id) REFERENCES users(id),
-    PRIMARY KEY (follower_id, followee_id)
+    FOREIGN KEY(follower_id) REFERENCES users(id),
+    FOREIGN KEY(followee_id) REFERENCES users(id),
+    PRIMARY KEY(follower_id, followee_id)
 );
 
-RENAME TABLE follow TO follows; 
+CREATE TABLE tags (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  tag_name VARCHAR(255) UNIQUE,
+  created_at TIMESTAMP DEFAULT NOW()
+);
 
-INSERT INTO follows (follower_id, followee_id) VALUES
-(1,2),
-(1,3),
-(3,1),
-(2,3);
-
-SELECT * FROM follows;
-
-INSERT INTO FOLLOWS (followee_id, followee_id) VALUES
-(2,1);
-
-CREATE TABLE tags(
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    tag_name VARCHAR(255) UNIQUE,
-    created_at TIMESTAMP DEFAULT NOW()
-)
-
-CREATE TABLE photo_tags(
+CREATE TABLE photo_tags (
     photo_id INTEGER NOT NULL,
     tag_id INTEGER NOT NULL,
     FOREIGN KEY(photo_id) REFERENCES photos(id),
     FOREIGN KEY(tag_id) REFERENCES tags(id),
     PRIMARY KEY(photo_id, tag_id)
 );
-
-INSERT INTO tags(tag_name) VALUES
-('adorable'),
-('cute'),
-('sunrise');
-
-SELECT * FROM tags;
-
-INSERT INTO photo_tags(photo_id, tag_id) VALUES
-(1,1),
-(1,2),
-(2,3),
-(3,2);
-
-SELECT * FROM photo_tags;
 
 
