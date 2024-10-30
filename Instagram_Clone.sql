1b3345c9ecb8024875a83c32deb3cc400ca55f47
CamilingJS <jonathancamiling@gmail.com>
2024-10-30 08:57:47 -0700

    created database and using it

--- Instagram_Clone.sql
+++ Instagram_Clone.sql
@@ -1,7 +1,13 @@
+CREATE DATABASE ig_clone;
+USE ig_clone; 
+
+
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
     created_at TIMESTAMP DEFAULT NOW()
 );
+
+
 
 
